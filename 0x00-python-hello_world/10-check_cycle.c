57eda97a91faea73d1a2ef4aa1851b076bf81ad0
Ken Mwangi <kenmwangi071@gmail.com>
2022-11-29 22:43:03 -0800

    10. Linked list cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,25 +1,30 @@
+#include <stdio.h>
 #include "lists.h"
 
 /**
  *   check_cycle - checks if a linked list contains a cycle
+ *   @list: singly linked list
  *   Return: 1 if the list has a cycle, 0 if it doesn't
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *slow = list;
-	listint_t *fast = list;
+	listint_t *head;
+	listint_t *tail;
 
 	if (!list)
 		return (0);
 
-	while (slow && fast && fast->next)
+	head = list;
+	tail = list;
+
+	while (tail != NULL && tail->next != NULL)
 	{
-		slow = slow->next;
-		fast = fast->next->next;
-		if (slow == fast)
+		head = head->next;
+		tail= tail->next->next;
+
+		if (head == tail)
 			return (1);
 	}
-
 	return (0);
 
 }
--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,13 +1,13 @@
-#ifdef LISTS_H
+#ifndef LISTS_H
 #define LISTS_H
 
 #include <stdlib.h>
 
 /**
- *  * struct listint_s - singly linked list
- *   * @next: points to the next node
- *    * Description: singly linked list node structure
- *     */
+ * struct listint_s - singly linked list.
+ * @n: integer.
+ * @next: points to the next node.
+ */
 typedef struct listint_s
 {
 	int n;
@@ -19,5 +19,4 @@ listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
 
-#endif
-
+#endif /* LISTS_H */
