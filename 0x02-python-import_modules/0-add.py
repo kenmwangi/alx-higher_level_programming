4d7b21c273358bc40c67911163eb942042568672
Ken Mwangi <kenmwangi071@gmail.com>
2022-11-30 21:33:54 -0800

    0. Import a simple function from a simple file

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+from task import add
+
+"""
+Addition function from file add_0.py
+
+Args: a: first integer, b: second integer
+
+Returns value. a + b
+"""
+
+a = 1
+b = 2
+
+if __name__ == "__main__":
+    print(f"{a:d} + {b:d} = {add(a, d):d}")
