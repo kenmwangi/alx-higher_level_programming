32b40090b0d02ced70bb8203bd8269bb427f2936
Ken Mwangi <kenmwangi071@gmail.com>
2022-12-02 00:56:53 -0800

    5. Can you C me now?

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    """
+    Removing all characters c and C from a string
+    """
+    dict = {ord(char): None for char in "cC"}
+
+    new_string = my_string.translate(dict)
+    return new_string
