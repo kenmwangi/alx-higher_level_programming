9abca711baed094839a59ebc374833c5095edab4
Ken Mwangi <kenmwangi071@gmail.com>
2022-12-02 01:16:26 -0800

    6. Lists of lists = Matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    """
+    print matrix of integers
+    """
+    if not matrix:
+        print()
+    else:
+        for row in matrix:
+            for item in row:
+                if row.index(item) != len(row) - 1:
+                    endspace = " "
+                else:
+                    endspace = " "
+                print("{:d}".format(item), end=endspace)
+            print()
+        
