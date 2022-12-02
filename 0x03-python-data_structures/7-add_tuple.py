c3ce9ec1e042eedb562de24b5bb26e45fa62f6a1
Ken Mwangi <kenmwangi071@gmail.com>
2022-12-02 02:32:47 -0800

    7. Tuples addition

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    """
+    add 2 tuples
+    Returns - tuple with 2 integers
+    """
+
+    if len(tuple_a) == 0:
+        tuple_a += (0, 0)
+    elif len(tuple_a) == 1:
+        tuple_a += (0, )
+    elif len(tuple_a) > 2:
+        tuple_a = tuple_a[:2]
+
+    if len(tuple_b) == 0:
+        tuple_b += (0, 0)
+    elif len(tuple_b) == 1:
+        tuple_b += (0, )
+    elif len(tuple_b) > 2:
+        tuple_b = tuple_b[:2]
+
+    # parallel iteration in tuple comprehension
+    sum_of_tuple = tuple(sum(item) for item in zip(tuple_a, tuple_b))
+    return sum_of_tuple
