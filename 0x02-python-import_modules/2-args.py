c8820f7e714b733c05d4590ea652594ffcd56b2d
Ken Mwangi <kenmwangi071@gmail.com>
2022-12-01 21:12:54 -0800

    2. How to make a script dynamic!

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,14 +8,13 @@ argv_length = len(argv) -1
 # The code will not run when file is imported
 if __name__ == "__main__":
     if argv_length > 1:
-        print(f"{argv_length:d} arguments:")
+        print("{:d} arguments:".format(argv_length))
     elif argv_length == 0:
-        print(f"{argv_length:d} arguments.")
+        print("{:d} arguments.".format(argv_length))
     else:
-        print(f"{argv_length:d} arguments:")
-
+        print("{:d} argument:".format(argv_length))
     # loop through and print item and index
     for arg_item in argv:
         if argv.index(arg_item) == 0:
             continue
-        print(f"{argv.index(arg_item):d}: {arg_item}")
+        print("{:d}: {}".format(argv.index(arg_item), arg_item))
