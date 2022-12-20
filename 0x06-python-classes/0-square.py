e59477a4a945571483e2c0d59b56983066314f17
Ken Mwangi <kenmwangi071@gmail.com>
2022-12-19 20:54:40 -0800

    0. My first square

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Square Class
+
+This module contains empty class defining the square
+
+Square = __import__('0-square').Square
+my_square = Square()
+
+"""
+class Square:
+    """
+    Blueprint of square
+
+    """
+    pass
