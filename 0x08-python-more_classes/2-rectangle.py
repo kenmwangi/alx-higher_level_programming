7eda76fb8cd992ae74cb96b2867a4b672e5c8f79
Ken Mwangi <kenmwangi071@gmail.com>
2023-01-04 00:10:57 -0800

    2. Area and Perimeter

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -22,7 +22,7 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """ Sets width private attribute value """
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
