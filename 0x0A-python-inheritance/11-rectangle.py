509a8e4740be26dba68d3ca1c953873b239edebb
Ken Mwangi <kenmwangi071@gmail.com>
2023-01-09 01:17:23 -0800

    11. Square #2

---
+++ 0x0A-python-inheritance/11-rectangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" module - base geometry """
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
+
+class Square(Rectangle):
+
+    """ Square inheriting from Rectangle"""
+
+    def __init__(self):
+        """ Initializing instance with args: wdth and height of rectangle """
+
+        self.integer_validator("size", size)
+        super().__init__size(size, size)
+        self.__size = size
+
+    def __str__(self):
+        """ Returns a formatted string """
+        return str("[Square] {}/{}".format(self.__size, self.__size))
+
+    def area(self):
+        """ area of instance Square """
+
+        return self.__size ** 2
