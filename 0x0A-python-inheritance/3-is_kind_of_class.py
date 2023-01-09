3e620a58e0d2b861276c0b96be519c3e6b178697
Ken Mwangi <kenmwangi071@gmail.com>
2023-01-09 00:21:12 -0800

    3. Same class or inherit from

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+""" module - finding instance of class from an object"""
+
+def is_kind_of_class(obj, a_class):
+    """ find if obj is instance of a class """
+    return isinstance(obj, a_class)
