b477ed57fa10475e29ac23a499d5a69b5de4cd0c
Ken Mwangi <kenmwangi071@gmail.com>
2023-01-10 00:25:10 -0800

    To JSON string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" module - return JSON representation of string """
+
+
+import json
+
+def to_json_string(my_obj):
+    """ JSON Representation """
+
+    return json.dumps(my_obj)
