46cccd73a7da0b03f8bad447942bf33a58dbe02f
Ken Mwangi <kenmwangi071@gmail.com>
2023-01-10 01:13:24 -0800

    11. Student to disk and reload

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Module 10-student.
+Creates a Student class.
+"""
+
+
+class Student:
+    """ public attributes, first_name, last_name, age """
+
+    def __init__(self, first_name, last_name, age):
+        """ initializing student instance """
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ dictionary representation of student instance """
+
+        my_dict = dict()
+        if type(attrs) is list and all(type(x) is str for xin attrs):
+            for x in attrs:
+                if x in self.__dict__:
+                    my_dict.update({x: self.__dict__[x]})
+            return my_dict
+        return self.__dict__.copy()
+
+    def reload_from_json(self, json):
+        """ replace all attributes of Student instance """
+
+        for x in json:
+            self.__dict__.update({x: json[x]})
